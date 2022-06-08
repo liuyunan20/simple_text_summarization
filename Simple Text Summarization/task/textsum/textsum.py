536a16d9fd2333a40d30e7d387c13d66ff4c1a2e
liuyunan <liuyunan.20@gmail.com>
2022-06-08 13:09:46 -0700

    Read an XML file with news texts. Extract the headers and the news texts. Process each news story: first, tokenize the text into separate sentences. Find N‾‾√ (N is the number of the source text's sentences). Tokenize sentences into words, convert the tokenized words to lowercase, and get rid of punctuation and stopwords. Lemmatize each word via the WordNetLemmatizer. Pass the collection of preprocessed sentences to the TfidfVectorizer (each standalone news story is as a separate dataset). Each sentence within one news story will represent a separate document (this way, the first story will comprise 16 documents in total, the second news story — 6 documents, and so on). Specify the word_tokenize() in the tokenizer parameter of the TfidfVectorizer: model = TfidfVectorizer(tokenizer=word_tokenize) Count the mean for each document in the resultant TF-IDF matrix (you can use numpy.mean() here). To access the term weights of a particular document, use the indexation; only those values that are greater than zero should be calculated. Find N‾‾√ best scoring sentences. Print the header and the selected sentences. Mind that you need to print the sentences in the order in which they appear in the original text. Repeat Steps 3-10 for each news story.

--- Simple Text Summarization/task/textsum/textsum.py
+++ Simple Text Summarization/task/textsum/textsum.py
@@ -6,56 +6,48 @@ from nltk.stem import WordNetLemmatizer
 import math
 from nltk.corpus import stopwords
 import string
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
 nltk.download('stopwords')
 
 
 def sent_imp(txt):
     lemmatizer = WordNetLemmatizer()
+    model = TfidfVectorizer(tokenizer=word_tokenize)
     lemma_freq = {}
     lemma_prob = {}
     sentences_total = []
     position = 0
     lemmas_total = []
     result = []
-    # lemmatize all words in text and calculate lemmas frequency
+    sents_in_news = []
+    # lemmatize words and build database
     sents = sent_tokenize(txt)
     sentence_num = round(math.sqrt(len(sents)))
     for sentence in sents:
+        lemmas_in_sent = []
         for word in word_tokenize(sentence):
             word = word.lower()
             if word not in stopwords.words('english') and word not in list(string.punctuation):
                 lemma = lemmatizer.lemmatize(word)
-                lemmas_total.append(lemma)
-                lemma_freq.setdefault(lemma, 0)
-                lemma_freq[lemma] += 1
-    # print(lemmas_total)
-
-    # process sentences
-    for sentence in sents:
-        lemmas_in_sent = []
-        prob_in_sent = []
-        for word in word_tokenize(sentence):
-            word = word.lower()
-            if word not in stopwords.words('english') and word not in list(string.punctuation):
-                lemma = lemmatizer.lemmatize(word.lower())
                 lemmas_in_sent.append(lemma)
-                lemma_prob.setdefault(lemma, lemma_freq[lemma]/len(lemmas_total))
-                prob_in_sent.append(lemma_prob[lemma])
-        sentences_total.append([position, statistics.mean(prob_in_sent), lemmas_in_sent, sentence])
+        sents_in_news.append(' '.join(lemmas_in_sent))
+        sentences_total.append([position, 0, ' '.join(lemmas_in_sent), sentence])
         position += 1
+    # print(len(sents_in_news))
+    model.fit(sents_in_news)
+    # tfidf_matrix = model.fit_transform(sents_in_news)
+    # print(tfidf_matrix)
+    # process sentences
+    for sentence in sentences_total:
+        vector = model.transform([sentence[2]])
+        # print(vector.shape)
+        term_weight = [x for x in vector.toarray()[0] if x > 0]
+        # print(term_weight)
+        sentence[1] = np.mean(term_weight)
 
-    while len(result) < sentence_num:
-        sentences_total.sort(key=lambda x: x[1], reverse=True)
-        highest_lemma = sorted(list(lemma_prob.items()), key=lambda x: x[1], reverse=True)[0][0]
-        for sentence in sentences_total:
-            if highest_lemma in sentence[2] and sentence not in result:  # sentence[2] stores a list of lemmas in sentence
-                result.append(sentence)
-                for lemma in sentence[2]:
-                    lemma_prob[lemma] = lemma_prob[lemma] ** 2
-                break
-        for sentence in sentences_total:
-            sentence[1] = statistics.mean([lemma_prob[x] for x in sentence[2]])
-
+    sentences_total.sort(key=lambda x: x[1], reverse=True)
+    result = sentences_total[:sentence_num]
     return [x[3] for x in sorted(list(result), key=lambda x: x[0])]
 
 
